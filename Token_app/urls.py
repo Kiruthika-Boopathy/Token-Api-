89a9918247962ab5ffebb80b7c48380a58de83b4
Kiruthika-Boopathy <kiruthika.b@vrdella.com>
2023-10-05 11:52:53 +0530

    update

--- Token_app/urls.py
+++ Token_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     path('fetch/',views.Fetch_data.as_view()),
-    path('insert/',views.InsertData.as_view())
+    path('insert/',views.InsertData.as_view()),
+    path('pagination/',views.InsertPageApiData.as_view())
 ]
--- Token_app/views.py
+++ Token_app/views.py
@@ -18,11 +18,11 @@ from rest_framework.permissions import IsAuthenticated
 
 
 class Fetch_data(ListAPIView):
-    # permission_classes = [IsAuthenticated]
-    # authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
     serializer_class = serializers.Serial
     queryset = Pagination.objects.all()
-    def get(self, request,*args,**kwargs):
+    def get(self,request,*args,**kwargs):
         query_set = Pagination.objects.all()
         serializer = serializers.Serial(query_set, many=True)
         user=User.objects.get(username="kiruthika")
@@ -108,3 +108,124 @@ class InsertData(views.APIView):
 
         return Response(data, status=status.HTTP_201_CREATED)
 
+#======================================================================================================================================
+
+#pagination#
+
+import concurrent.futures
+import logging
+import random
+import string
+from concurrent.futures import ThreadPoolExecutor
+
+import requests
+from django.db import connection
+from rest_framework import views, status
+from rest_framework.response import Response
+import threading
+import json
+import time
+from adrf import views
+
+from . import serializers
+
+
+class InsertPageApiData(views.APIView):
+    def get(self, request):
+        start = time.time()
+        logger = logging.getLogger( __name__ )
+
+        def my_function():
+            try:
+
+                pass # This will raise a ZeroDivisionError
+            except Exception as e:
+                # Log the error message
+                logger.error( 'An error occurred: %s', str( e ) )
+
+        def insert_data(data):
+            with connection.cursor() as cursor:
+                api_data = data
+                for item in api_data:
+                    raw_query = """
+                                INSERT INTO pagination (api_id, id, name, tagline, first_brewed, description, image_url, abv, ibu, target_fg, target_og, ebc, srm, ph, attenuation_level, volume, boil_volume, method, ingredients, food_pairing,
+                                brewers_tips, contributed_by) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+                    params = (generate_api_id(),
+                              item["id"],
+                              item["name"],
+                              item["tagline"],
+                              item["first_brewed"],
+                              item["description"],
+                              item["image_url"],
+                              item["abv"],
+                              item["ibu"],
+                              item["target_fg"],
+                              item["target_og"],
+                              item["ebc"],
+                              item["srm"],
+                              item["ph"],
+                              item["attenuation_level"],
+                              str(item["volume"]),
+                              str( item["boil_volume"] ),
+                              str( item["method"] ),
+                              str( item["ingredients"] ),
+                              str( item["food_pairing"] ),
+                              item["brewers_tips"],
+                              item["contributed_by"]
+                              )
+                    cursor.execute( raw_query, params )
+                # connection.commit()
+
+        def generate_api_id():
+            with connection.cursor() as cursor:
+                api_id = "API" + "".join( random.choice( string.digits ) for i in range( 6 ) )
+                cursor.execute( "SELECT api_id FROM pagination WHERE api_id=%s", (api_id,) )
+                db_id = cursor.fetchone()
+                if not db_id:
+                    return api_id
+                else:
+                    api_id = generate_api_id()
+                    return api_id
+
+        def fetch_data(url):
+            response = requests.get( url )
+            if response.status_code == 200:
+                try:
+                    data = response.json()
+                    insert_data( data )
+                    return {"message": "success"}
+                except json.JSONDecodeError as e:
+                    logger.error( f'Failed to decode JSON data from {url}: {e}' )
+                    return {'error': f'Failed to decode JSON data from {url}: {e}'}
+            else:
+                logger.error(
+                    f'Failed to retrieve data from {url}, status code: {response.status_code}, content: {response.content}' )
+                return {'error': f'Failed to retrieve data from {url}, status code: {response.status_code}'}
+
+        api_urls = [
+             # 'https://api.punkapi.com/v2/beers/',
+             # 'https://api.punkapi.com/v2/beers/2',
+               'https://api.punkapi.com/v2/beers/3'
+        ]
+
+        def page(url):
+            for i in range( 1, 11 ):
+                api_url = str( url ) + str( i )
+                fetch_data( api_url )
+
+        # Use a ThreadPoolExecutor with a thread barrier
+        num_threads = len( api_urls )
+        with concurrent.futures.ThreadPoolExecutor( max_workers=num_threads ) as executor:
+            # Create a thread barrier to synchronize threads
+            barrier = threading.Barrier( num_threads )
+
+            # Submit tasks to the executor
+            futures = [executor.submit(page(url)) for url in api_urls]
+
+
+            concurrent.futures.wait( futures )
+
+        end = time.time()
+        tot = end - start
+        val = {"total": tot, "data": "Data inserted successfully"}
+        return Response( val, status=status.HTTP_201_CREATED )
